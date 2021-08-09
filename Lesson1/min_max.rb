b75656856f573117ea6eca852cc15d986c6c06a5
AgentSmith <vip1313@gmail.com>
2021-08-09 12:03:21 +0300

    fix

--- Lesson1/min_max.rb
+++ Lesson1/min_max.rb
@@ -1,2 +1,6 @@
-puts "Максимальное слово: #{IO.foreach('tekst.txt').max_by(&:length)}"
- puts "Минимальное слово: #{IO.foreach('tekst.txt').min_by(&:length)}"
+# frozen_string_literal: true
+
+max = IO.foreach('tekst.txt').map(&:chomp).max_by(&:length)
+min = IO.foreach('tekst.txt').map(&:chomp).min_by(&:length)
+puts "Максимальное слово: #{max} (#{max.size})"
+puts "Минимальное слово: #{min} (#{min.size})"
