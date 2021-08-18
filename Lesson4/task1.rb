0cbb969875ad8e7f63bfcb1b97838534b1224ee9
AgentSmith <vip1313@gmail.com>
2021-08-18 23:59:53 +0300

    create 4 lesson

---
+++ Lesson4/task1.rb
@@ -0,0 +1,42 @@
+class Group
+  include Enumerable
+  attr_accessor :kolect
+   def initialize
+    @kolect = []
+   end
+   def <<(param)
+    kolect << param
+  end
+   def each(&block)
+    kolect.each(&block)
+   end
+
+class User
+  attr_accessor :name, :profesion
+  def initialize(name:, profesion:)
+    @name = name
+    @profesion = profesion
+  end
+end
+end
+t = Group.new
+t << Group::User.new(
+  name: 'Иванов Иван Иванович',
+  profesion: 'Team Leader'
+)
+
+ 
+t << Group::User.new(
+  name: 'Петров Петр Петрович',
+  profesion: 'Backend Developer'
+)
+
+  
+t << Group::User.new(
+  name: 'Олегов Олег Олегович',
+  profesion: 'Frontend Developer'
+)
+
+
+  kolect = t.map { |u| "#{u.name} (#{u.profesion})" }
+ puts kolect
