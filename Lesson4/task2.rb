e7c0d5ff4bd60f59c2386a61590bd5ff18a38d7b
AgentSmith <vip1313@gmail.com>
2021-08-19 01:18:23 +0300

    fix

--- Lesson4/task2.rb
+++ Lesson4/task2.rb
@@ -1,16 +1,23 @@
+# frozen_string_literal: true
+
 class News
   include Comparable
   attr_accessor :title, :body, :publicate_date
-def initialize (title:, body:, publicate_date:)
-  @title = title
-  @body = body
-  @publicate_date = publicate_date
-end
 
-def <=>(perm)
-  publicate_date <=> perm
+  def initialize(title:, body:, publicate_date:)
+    @title = title
+    @body = body
+    @publicate_date = publicate_date
+  end
+
+  def <=>(other)
+    publicate_date <=> other
+  end
 end
-end
-news_one = News.new(title: 'Тунгусский метеорит', body: 'упал метеорит', publicate_date: Time.mktime(1908, 6, 17) )
-news_two = News.new(title: 'Тунгусский метеорит', body: 'Наконец его нашли', publicate_date: Time.mktime(2022, 8, 18) )
+news_one = News.new(title: 'Тунгусский метеорит', body: 'упал метеорит', publicate_date: Time.mktime(1908, 6, 17))
+news_two = News.new(title: 'Тунгусский метеорит', body: 'Наконец его нашли', publicate_date: Time.mktime(2022, 8, 18))
+news_thr = News.new(title: 'Тунгусский метеорит', body: 'оказался летающим кораблем',
+                    publicate_date: Time.mktime(2023, 8, 18))
 p news_one >= news_two
+puts news_one <= news_two
+puts news_two == news_thr
