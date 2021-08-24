df58fbcb6cd05892c912079257c9347e0a9cd29d
AgentSmith <vip1313@gmail.com>
2021-08-24 23:24:02 +0300

    fix rubocop

--- Lesson6/task4.rb
+++ Lesson6/task4.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
+
 class Array
-def for(num = nil)
-  i = 0
-  loop do
-    break unless block_given?
-    yield self unless num.instance_of?(Integer) && num <= 0
-    i += 1
-    break if i >= num.to_i && num != nil
+  def for(num = nil)
+    i = 0
+    loop do
+      break unless block_given?
+
+      yield self unless num.instance_of?(Integer) && num <= 0
+      i += 1
+      break if i >= num.to_i && !num.nil?
+    end
   end
-end
 end
 
 [1, 2, 3].for(5) { |x| puts x }
