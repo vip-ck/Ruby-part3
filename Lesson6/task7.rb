113e9e83a8fc3c9cfd8254ebaf66e591d766387b
AgentSmith <vip1313@gmail.com>
2021-08-24 23:53:15 +0300

    create 7 task

---
+++ Lesson6/task7.rb
@@ -0,0 +1,11 @@
+class Integer
+  def to_a(bit = 2)
+    
+    bit = 2 unless [2, 8, 16].include? bit
+    arr = to_s(bit).split('')
+    bit < 16 ? arr.map(&:to_i) : arr
+  end
+end
+
+p 10.to_a(8)
+p 10.to_a(16)
