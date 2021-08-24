ac702162cb6e617d3505e3429d88e07f2eeab6d4
AgentSmith <vip1313@gmail.com>
2021-08-24 23:36:50 +0300

    create 5 task

---
+++ Lesson6/task5.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/EachWithObject
+# rubocop:disable Style/Semicolon
+print [1, 2, 3, 4, 5].reduce([]) { |arr, el| arr << el if el.odd?; arr }
+
