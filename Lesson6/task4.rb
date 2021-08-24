021023003bdd79346421ffc992b37fdbeb9c4ca1
AgentSmith <vip1313@gmail.com>
2021-08-24 23:33:06 +0300

    fix

--- Lesson6/task4.rb
+++ Lesson6/task4.rb
@@ -5,12 +5,13 @@ class Array
     i = 0
     loop do
       break unless block_given?
-
-      yield self unless num.instance_of?(Integer) && num <= 0
+each do |x|
+      yield x unless num.instance_of?(Integer) && num <= 0
+end
       i += 1
       break if i >= num.to_i && !num.nil?
     end
   end
 end
 
-[1, 2, 3].for(5) { |x| puts x }
+[1, 2, 3].for(5) { |x| p x }
