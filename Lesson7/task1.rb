140a3ec70af23931b9a8e08ee0cf38877d8d83f8
AgentSmith <vip1313@gmail.com>
2021-08-25 23:44:55 +0300

    create task 1 i task2

---
+++ Lesson7/task1.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+COUNTRY = {
+  Germany: '83,124,418',
+  France: '64,990,511',
+  Italy: '60,627,291',
+  Spain: '46,692,858',
+  Ukraine: '44,246,156',
+  Poland: '37,921,592',
+  Romania: '19,506,114',
+  Netherlands: '17,059,560',
+  Greece: '10,522,246',
+  Switzerland: '8,525,611'
+}.freeze
+puts COUNTRY
